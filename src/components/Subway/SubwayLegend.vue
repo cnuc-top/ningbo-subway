ed9b91bb9f27c2e14a0c7da552bd9c6bc4aefca5
xank <xank@qq.com>
2018-05-29 23:47:22 +0800

    init vue

---
+++ src/components/Subway/SubwayLegend.vue
@@ -0,0 +1,25 @@
+<style lang='stylus'>
+</style>
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  props:{
+  },
+
+  data() {
+    return {
+    }
+  },
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+}
+</script>
